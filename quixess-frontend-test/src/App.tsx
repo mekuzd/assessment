fc4b5370bf92b0d395971b1ebe2a33e1c4fa5751
Emeka <101795048+mekuzd@users.noreply.github.com>
2025-07-31 14:24:20 +0100

    instal tailwind dependecy and react

---
+++ quixess-frontend-test/src/App.tsx
@@ -0,0 +1,13 @@
+
+import './App.css'
+import './index.css'
+function App() {
+
+  return (
+ <div className="text-4xl text-red-500 text-center mt-10">
+      Tailwind is working!
+    </div>
+  )
+}
+
+export default App
